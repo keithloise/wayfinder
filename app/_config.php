26f2b8fe85ccced912de382b146ad3ef39244168
Keith L <k8t_loise16@yahoo.com>
2021-07-27 15:22:11 +1200

    Initial commit

---
+++ app/_config.php
@@ -0,0 +1,10 @@
+<?php
+
+use SilverStripe\Security\PasswordValidator;
+use SilverStripe\Security\Member;
+
+// remove PasswordValidator for SilverStripe 5.0
+$validator = PasswordValidator::create();
+// Settings are registered via Injector configuration - see passwords.yml in framework
+Member::set_password_validator($validator);
+date_default_timezone_set('Pacific/Auckland');
